a5b3e478fad2ddbe37f67805ba76517741f6f19c
Ajay0496 <TestAutomation2.Ajay@gmail.com>
2024-12-01 18:10:02 +0530

    ECP1Testt02 Arthimetic Operations Test Commit for Addition

---
+++ src/test/java/eclipsedemo/EclipseJavaProgramsTest/ArithmeticOperationsTest.java
@@ -0,0 +1,27 @@
+package eclipsedemo.EclipseJavaProgramsTest;
+
+import java.util.Scanner;
+
+public class ArithmeticOperationsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();  
+
+
+        double sum = add(num1, num2);
+        System.out.println("Sum:  " + sum);
+        scanner.close();
+	}
+	public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+}
