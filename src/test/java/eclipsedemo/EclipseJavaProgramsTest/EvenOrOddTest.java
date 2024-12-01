60e69212a51d9bde4c761767f30f6734647da2e1
AjayYadav1804 <TestAutomation1.Ajay@gmail.com>
2024-12-01 17:55:24 +0530

    P2User Even Odd Commit

---
+++ src/test/java/eclipsedemo/EclipseJavaProgramsTest/EvenOrOddTest.java
@@ -0,0 +1,21 @@
+package eclipsedemo.EclipseJavaProgramsTest;
+
+import java.util.Scanner;
+
+public class EvenOrOddTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner e = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int n = e.nextInt();
+		if (n % 2 == 0) {
+		System.out.println("Even number");
+		} else {
+		System.out.println("Odd number");
+		}
+		
+	}
+
+}
