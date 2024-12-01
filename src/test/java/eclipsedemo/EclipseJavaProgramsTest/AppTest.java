01e4ee3ff7e8698c30f797ab411b79f0c4b61271
Ajay0496 <TestAutomation2.Ajay@gmail.com>
2024-12-01 15:56:08 +0530

    Initial Commit T1

---
+++ src/test/java/eclipsedemo/EclipseJavaProgramsTest/AppTest.java
@@ -0,0 +1,13 @@
+package eclipsedemo.EclipseJavaProgramsTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
