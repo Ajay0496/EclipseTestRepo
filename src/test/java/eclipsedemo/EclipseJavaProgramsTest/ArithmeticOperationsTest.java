7cae6fa7164901b5533f351e29e3136751e18ec8
Ajay0496 <TestAutomation2.Ajay@gmail.com>
2024-12-01 18:34:48 +0530

    commit Multiply Operation by ECP1Testt02

--- src/test/java/eclipsedemo/EclipseJavaProgramsTest/ArithmeticOperationsTest.java
+++ src/test/java/eclipsedemo/EclipseJavaProgramsTest/ArithmeticOperationsTest.java
@@ -15,14 +15,32 @@ public class ArithmeticOperationsTest {
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();  
 
-
         double sum = add(num1, num2);
+
         System.out.println("Sum:  " + sum);
         System.out.println("Sum:  " + sum);
+
+        double difference = subtract(num1, num2);
+        System.out.println("Difference: " + difference);
+        
+        double product = multiply(num1, num2);
+        System.out.println("Product: " + product);
         scanner.close();
+       
+        
 	}
+	public static double subtract(double num1, double  
+			 num2) {
+			        return num1 - num2;
+			    }
 	public static double add(double num1, double num2) {
         return num1 + num2;
     }
+	
+	public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+	
+	
 
 }
