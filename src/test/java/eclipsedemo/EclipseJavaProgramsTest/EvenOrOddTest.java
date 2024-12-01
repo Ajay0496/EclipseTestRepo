800ab675d2ec713517043e3fbb7f94fd869f82ae
Ajay0496 <TestAutomation2.Ajay@gmail.com>
2024-12-01 17:57:09 +0530

    Commit P2 user Even Or Odd

--- src/test/java/eclipsedemo/EclipseJavaProgramsTest/EvenOrOddTest.java
+++ src/test/java/eclipsedemo/EclipseJavaProgramsTest/EvenOrOddTest.java
@@ -11,9 +11,9 @@ public class EvenOrOddTest {
 		System.out.println("Enter a Number");
 		int n = e.nextInt();
 		if (n % 2 == 0) {
-		System.out.println("Even number");
+		System.out.println("Even number ");
 		} else {
-		System.out.println("Odd number");
+		System.out.println("Odd number ");
 		}
 		
 	}
